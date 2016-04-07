086d8fa35912930d6d0b603a31e24cfc904a45a1
kortirso <kortirso@gmail.com>
2016-04-07 17:04:20 +0300

    add checks for parsing

--- app/jobs/parsing_task_job.rb
+++ app/jobs/parsing_task_job.rb
@@ -3,37 +3,43 @@ class ParsingTaskJob < ActiveJob::Base
     queue_as :default
 
     def perform(task)
-        parser = Parser::TaskParser.new
+        task_parser = Parser::TaskParser.new
         task.words.each do |word|
-            page = parser.parsing(word.name)
+            page = task_parser.parsing(word.name)
             if page.code.to_i == 200
-                parser = page.parser
+                for_parse = page.parser
 
                 #top adwords
-                i = parser.at_css('div#Z_Ltg ol').children.count
-                (1..i).each do |x|
-                    link_text = parser.at_css("div#Z_Ltg ol li:nth-of-type(#{x}) cite").text
-                    word.links.create(type: 'top', name: link_text)
+                object = for_parse.at_css('div#Z_Ltg ol')
+                if object
+                    i = object.children.count
+                    (1..i).each do |x|
+                        temp = for_parse.at_css("div#Z_Ltg ol li:nth-of-type(#{x}) cite")
+                        word.links.create(type: 'top', name: temp.text) if temp
+                    end
                 end
 
                 #non adwords
-                j = parser.at_css('div#ires ol').children.count
-                (1..j).each do |x|
-                    object = parser.at_css("div#search div#ires ol .g:nth-of-type(#{x}) .s .kv cite")
-                    word.links.create(type: 'none', name: object.text) unless object.nil?
+                object = for_parse.at_css('div#ires ol')
+                if object
+                    j = object.children.count
+                    (1..j).each do |x|
+                        temp = for_parse.at_css("div#search div#ires ol .g:nth-of-type(#{x}) .s .kv cite")
+                        word.links.create(type: 'none', name: temp.text) if temp
+                    end
                 end
 
                 # save html to file
                 filename = "#{Rails.root}/public/uploads/task/file/#{word.task_id}/#{word.id}.html"
                 file = File.new(filename, 'w')
-                file.puts parser
+                file.puts for_parse
                 file.close
 
-                total_results = parser.at_css('div#resultStats').children[0].text
+                total_results = for_parse.at_css('div#resultStats').children[0].text
                 total_links = page.links.count
 
                 word.update(complete: true, total_results: total_results, total_links: total_links, html_result: filename)
-                sleep(20)
+                sleep(10)
             else
                 sleep(60)
             end
